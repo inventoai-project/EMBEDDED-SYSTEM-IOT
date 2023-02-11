71f0768998fe242a0b0cbb23b21d9c19f1115d0a
Inventoai <125119155+inventoai-project@users.noreply.github.com>
2023-02-11 18:01:22 +0530

    Add files via upload

---
+++ IoT device connected the wifi thriough gateway/IoT device connected the wifi thriough gateway.ino
@@ -0,0 +1,280 @@
+//  iot device connected the wifi through gateway SSID and Password Push propcess by Arghyadeep Debnath
+//  Inventoai  websitelink https://inventoai.blogspot.com       official email : inventoai@outlook.com
+//  Switable for the device ESP8266 SoC
+//  Gateway login based configuration
+//  Reset connection through push button by pressing 1 second on the device pairing switch which setb on the D0 / GPIO16 PIN
+//  To make the reset circuit take a 3.3 +V, a GND and a input pin on GPIO16; 
+//  A push button switch need to connect on 3.3v , output of the switch need to connect on GPIO16,
+//  A 10Kohm resistance need to connect after the switch -GPIO16  and GND pin.
+
+
+
+//  MIT License
+
+//  Copyright (c) [2023] [INVENTOAI]
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+
+//Variables
+int i = 0;
+int statusCode;
+const char* ssid = "Default SSID";
+const char* passphrase = "Default passord";
+String st;
+String content;
+String esid;
+String epass = "";
+
+
+//Function Decalration
+bool testWifi(void);
+void launchWeb(void);
+void setupAP(void);
+
+
+//Establishing Local server at port 80
+ESP8266WebServer server(80);
+void setup()
+{
+  Serial.begin(115200); //Initialising if(DEBUG)Serial Monitor
+  Serial.println();
+  Serial.println("Disconnecting current wifi connection");
+  WiFi.disconnect();
+  EEPROM.begin(512); //Initialasing EEPROM
+  delay(10);
+  pinMode(D0, INPUT);
+  Serial.println();
+  Serial.println();
+  Serial.println("Startup");
+
+
+  //---------------------------------------- Read eeprom for ssid and pass
+  Serial.println("Reading EEPROM ssid");
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  Serial.println();
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
+  Serial.print("PASS: ");
+  Serial.println(epass);
+  WiFi.begin(esid.c_str(), epass.c_str());
+}
+void loop() {
+  if ((WiFi.status() == WL_CONNECTED))
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      Serial.print("Connected to ");
+      Serial.print(esid);
+      Serial.println(" Successfully");
+      delay(100);
+    }
+  }
+  else
+  {
+  }
+  if (testWifi() && (digitalRead(D0) != 1))
+  {
+    Serial.println(" connection status positive");
+    return;
+  }
+  else
+  {
+    Serial.println("Connection Status Negative / D0 HIGH");
+    Serial.println("Turning the HotSpot On");
+    launchWeb();
+    setupAP();// Setup HotSpot
+  }
+  Serial.println();
+  Serial.println("Waiting.");
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    Serial.print(".");
+    delay(100);
+    server.handleClient();
+  }
+  delay(1000);
+}
+
+
+//----------------------------------------------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change
+bool testWifi(void)
+{
+  int c = 0;
+  //Serial.println("Waiting for Wifi to connect");
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+
+      
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+
+      
+      //Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+
+    
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+    st += ")";
+    
+    //st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  //INVENTOAI IoT Door rev.1.0" is the generated hotspot which is generated by the ESP8266 and the password "Invento" , if the password are removed it become the password less
+  WiFi.softAP("INVENTOAI IoT Door rev.1.0", "Inventoai@2023.robo");
+  Serial.println("Initializing_softap_for_wifi credentials_modification");
+  launchWeb();
+  Serial.println("over");
+}
+void createWebServer()
+{
+  {
+    server.on("/", []() {
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html><h1><center>Inventoai IoT Smart Door rev.1.0 Configuration page</center><h1>";
+      content += "<h1><center><form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form></center></h1>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><center><form method='get' action='setting'><label><p>SSID:         Password:</p></label><input name='ssid' length=32><input name='pass' length=64><p><input type='submit'></p></form></font></center>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+        ESP.restart();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+    });
+  }
+}
